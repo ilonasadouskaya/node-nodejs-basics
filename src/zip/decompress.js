bbf884ebf6d3cad77d4a4175cd91f625b9be7933
Ilona Sadowska <ilona.sadowska.mail@gmail.com>
2025-04-28 23:15:37 +0200

    feat: complete `zip` tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const resource = fs.createReadStream('src/zip/files/archive.gz');
+    const destination = fs.createWriteStream('src/zip/files/fileToCompress.txt');
+
+    const decompress = createGunzip();
+    pipeline(resource, decompress, destination, (err) => console.error(err));
 };
 
 await decompress();
