d8198f1f679db277e9f3b604ac56994ada2ad614
Ilona Sadowska <ilona.sadowska.mail@gmail.com>
2025-04-28 22:49:56 +0200

    feat: complete `cli` tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+const envStartsWith = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const filteredEnvVars = Object.keys(process.env).filter(key => key.startsWith(envStartsWith));
+    const result = filteredEnvVars.map(key => `${key}=${process.env[key]}`).join(';');
+
+    console.log(result)
 };
 
 parseEnv();
